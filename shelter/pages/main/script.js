c3840ac5efce626094f0b0edc0f78c6a242d2439
Dzianis Salahub <antigon2012@gmail.com>
2022-04-26 03:46:02 +0300

    feat: implement burger menu

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,33 @@
+const MENU_BTN = document.querySelector(".menu-burger-header");
+const MAIN_NAV = document.querySelector(".main-nav");
+const HEADER_LOGO = document.querySelector(".header-logo");
+const FADE_LAYER = document.querySelector(".mobile-menu-wrap");
+const MAIN_WRAPPER = document.querySelector(".wrapper");
+const NAV_ITEM = document.querySelectorAll(".nav-item");
+
+function openMenu() {
+  FADE_LAYER.classList.toggle("active");
+  MENU_BTN.classList.toggle("active");
+  MAIN_NAV.classList.toggle("active");
+  HEADER_LOGO.classList.toggle("open");
+  // MAIN_WRAPPER.classList.toggle("active");
+}
+
+function closeMenu() {
+  FADE_LAYER.classList.remove("active");
+  MENU_BTN.classList.remove("active");
+  MAIN_NAV.classList.remove("active");
+  HEADER_LOGO.classList.remove("open");
+  // MAIN_WRAPPER.classList.remove("active");
+}
+
+// open menu
+MENU_BTN.addEventListener("click", openMenu);
+
+// close menu in the link
+for (let i = 0; i < NAV_ITEM.length; i++) {
+  NAV_ITEM[i].addEventListener("click", closeMenu);
+}
+
+FADE_LAYER.addEventListener("click", closeMenu);
+// NAV_LIST.addEventListener("click", (e) => e.stopPropagation());
