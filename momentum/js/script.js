b0b88f05add0e58fa25753614e7cc8143b2fbdcd
Dzianis Salahub <antigon2012@gmail.com>
2022-08-06 17:33:38 +0300

    feat: implement image slider

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,8 +1,10 @@
 import { showTime } from "./time.js";
 import { getLocalStorage, setLocalStorage } from "./localStorage.js";
-import { setBg } from "./slider.js";
+import { setBg, getSlideNext, getSlidePrev } from "./slider.js";
 
 showTime();
 window.addEventListener("load", getLocalStorage);
 window.addEventListener("beforeunload", setLocalStorage);
+document.querySelector(".slide-prev").addEventListener("click", getSlidePrev);
+document.querySelector(".slide-next").addEventListener("click", getSlideNext);
 setBg();
--- momentum/js/slider.js
+++ momentum/js/slider.js
@@ -1,4 +1,5 @@
 import { getTimeOfDay } from "./greeting.js";
+export { setBg, getSlideNext, getSlidePrev };
 
 const body = document.body;
 
@@ -6,11 +7,29 @@ const getRandomNum = (min = 1, max = 20) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
+let randomNum = getRandomNum();
+
 const setBg = () => {
   let timeOfDay = getTimeOfDay();
-  let bgNum = (getRandomNum() + "").padStart(2, "0");
+  let bgNum = (randomNum + "").padStart(2, "0");
 
   body.style.backgroundImage = `url('https://raw.githubusercontent.com/antigon2012/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
 };
 
-export { setBg };
+const getSlideNext = () => {
+  if (randomNum === 20) {
+    randomNum = 1;
+  } else {
+    randomNum = randomNum + 1;
+  }
+  setBg();
+};
+
+const getSlidePrev = () => {
+  if (randomNum === 1) {
+    randomNum = 20;
+  } else {
+    randomNum = randomNum - 1;
+  }
+  setBg();
+};
